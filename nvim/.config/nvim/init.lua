8c3727b917a25144471103c70ad621b198d8cb42
Lucas Reis <dev.lucasreis26@gmail.com>
2025-06-30 00:06:00 -0300

    feat: jj now esc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,6 @@ vim.keymap.set('n','<c-h>',':wincmd h<CR>')
 
 require('config.lazy')
 vim.cmd([[colorscheme catppuccin-frappe]]);
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cindent = true
@@ -19,6 +18,7 @@ vim.keymap.set('t','<esc><esc>','<c-\\><c-n>')
 vim.keymap.set('v','<leader>y','"+y')
 vim.keymap.set('n','<leader>yy','"+yy')
 vim.keymap.set('n','<leader>p','"+p')
+vim.keymap.set('i','jj','<esc>')
 
 vim.api.nvim_create_autocmd("TermOpen",{
 	pattern="*",

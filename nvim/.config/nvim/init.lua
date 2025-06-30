c269b6986e2fd895bf5907c9e4d5df70ce60ef11
Lucas Reis <dev.lucasreis26@gmail.com>
2025-06-30 00:40:02 -0300

    feat: indent key

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -19,6 +19,7 @@ vim.keymap.set('v','<leader>y','"+y')
 vim.keymap.set('n','<leader>yy','"+yy')
 vim.keymap.set('n','<leader>p','"+p')
 vim.keymap.set('i','jj','<esc>')
+vim.keymap.set('n','<leader>=','gg0vG$=')
 
 vim.keymap.set('n','<esc>',function()
 	if vim.v.hlsearch == 1 then

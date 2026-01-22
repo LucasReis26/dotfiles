a866df8e2a21c1dba061859c1a793f06f6219324
Lucas Reis <dev.lucasreis26@gmail.com>
2026-01-22 16:41:10 -0300

    new features

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -4,43 +4,50 @@ return {
 	lazy = true,
 	ft = "markdown",
 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-		--   -- refer to `:h file-pattern` for more examples
-		--   "BufReadPre path/to/my-vault/*.md",
-		--   "BufNewFile path/to/my-vault/*.md",
-		-- },
-		dependencies = {
-			-- Required.
-			"nvim-lua/plenary.nvim",
+	cmd = {
+        "ObsidianToday",
+        "ObsidianNew",
+        "ObsidianQuickSwitch",
+        "ObsidianSearch",
+        "ObsidianWorkspace",
+    },
+	event = {
+		-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+		-- refer to `:h file-pattern` for more examples
+		"BufReadPre /home/cinema/obsidian/*.md",
+		"BufNewFile /home/cinema/obsidian/*.md",
+	},
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
 
-			-- see below for full list of optional dependencies 👇
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "personal",
+				path = "~/obsidian",
+			},
+
+			-- other fields ...
+
+
 		},
-		opts = {
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/hd/Insync/luctreis@gmail.com/OneDrive/obsidian",
-				},
 
-				-- other fields ...
-
-
-			},
-
-			templates = {
-				folder = "./Assets/Templates",
-				date_format = "%Y-%m-%d-%a",
-				time_format = "%H:%M",
-			},
-			daily_notes={
-				folder="Vida Pessoal/Daily Notes",
-			},
-			ui = { 
-				enable = false 
-			},
-			disable_frontmatter = true,
-			-- see below for full list of options 👇
+		templates = {
+			folder = "./Assets/Templates",
+			date_format = "%Y-%m-%d-%a",
+			time_format = "%H:%M",
 		},
-	}
+		daily_notes={
+			folder="Vida Pessoal/Daily Notes",
+		},
+		ui = { 
+			enable = false 
+		},
+		disable_frontmatter = true,
+		-- see below for full list of options 👇
+	},
+}

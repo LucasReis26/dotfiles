fce5fccdaa8fcc1c730c15db6097ae3ca177adbb
Lucas Reis <dev.lucasreis26@gmail.com>
2026-02-14 09:53:40 -0300

    improving fold methods

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.keymap.set('n','<c-l>',':wincmd l<CR>')
 vim.keymap.set('n','<c-k>',':wincmd k<CR>')
 vim.keymap.set('n','<c-j>',':wincmd j<CR>')
 vim.keymap.set('n','<c-h>',':wincmd h<CR>')
+-- vim.opt.foldmethod = 'marker'
 
 require('config.lazy')
 require('commands.options')

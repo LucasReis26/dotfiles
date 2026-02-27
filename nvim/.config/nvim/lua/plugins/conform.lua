f0299c948837de6ae5cf3ac3fa5afe39c39fcebf
Lucas Reis <dev.lucasreis26@gmail.com>
2026-02-27 08:49:13 -0300

    feat: html format added

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
 		formatters_by_ft = {
 			javascript = {"prettierd", "prettier", stop_after_first = true},
 			typescriptreact = {"prettierd", "prettier", stop_after_first = true},
+			html = {"prettierd", "prettier", stop_after_first = true},
 			java = {"google-java-format"}
 		},
 		format_on_save = {

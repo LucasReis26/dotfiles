2c5081c2d3b56361bc97beea765415962a3ce87d
Lucas Reis <dev.lucasreis26@gmail.com>
2026-02-27 08:42:07 -0300

    fix: conform desabilitado
    Conform estava desabilitado e aproveitei pra adicionar um novo plugin Mason.nvim para gerenciar pacotes no nvim

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,11 @@
 return {
 	'stevearc/conform.nvim',
-	enabled = false,
+	enabled = true,
 	opts = {
 		formatters_by_ft = {
 			javascript = {"prettierd", "prettier", stop_after_first = true},
-			typescriptreact = {"prettierd", "prettier", stop_after_first = true}
+			typescriptreact = {"prettierd", "prettier", stop_after_first = true},
+			java = {"google-java-format"}
 		},
 		format_on_save = {
 			timeout_ms = 500,

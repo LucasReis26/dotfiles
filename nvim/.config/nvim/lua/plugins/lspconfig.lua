b40624dcfb604b357691609443109fae713dc4a8
Lucas Reis <dev.lucasreis26@gmail.com>
2026-02-28 12:34:21 -0300

    feat: completions for c and html

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -11,6 +11,31 @@ return{
 			cmd = { "jdtls" },
 			root_dir = vim.fs.root(0,{".git", "pom.xml", "gradlew", "build.gradle"}),
 		}
+		vim.lsp.config.emmet_ls = {
+			capabilities = capabilities,
+			filetypes = {
+				"html",
+				"typescriptreact",
+				"javascriptreact",
+				"css",
+				"markdown",
+			}
+		}
+		vim.lsp.config.clangd = {
+			capabilities = capabilities,
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--clang-tidy",
+				"--header-insertion=iwyu",
+				"--completion-style=detailed",
+				"--function-arg-placeholders",
+				"--fallback-style=llvm"
+			},
+			root_dir = vim.fs.root(0, { ".git", "compile_commands.json", "compile_flags.txt", ".clangd" }),
+		}
+		vim.lsp.enable('clangd')
+		vim.lsp.enable('emmet_ls')
 		vim.lsp.enable('jdtls')
 		
 	end,

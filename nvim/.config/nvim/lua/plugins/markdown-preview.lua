90bd201fa6c7a60e5977fbd430a8c7d377f53a8e
Lucas Reis <dev.lucasreis26@gmail.com>
2026-06-24 12:00:43 -0300

    fix: markdown preview image link

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,16 +1,18 @@
 return{
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && yarn install",
-	version = "*",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-	config = function()
-		vim.cmd([[
-		let g:mkdp_browser = ''
-		nmap <C-p> <Plug>MarkdownPreviewToggle
-		]])
-	end
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  init = function()
+    -- 1. Criamos a função em Vimscript para o Neovim separar os argumentos
+    vim.cmd([[
+      function! OpenZenFlatpak(url)
+        call jobstart(['flatpak', 'run', 'app.zen_browser.zen', a:url])
+      endfunction
+    ]])
+    
+    -- 2. Dizemos ao plugin para ignorar o navegador padrão e usar a nossa função
+    vim.g.mkdp_browserfunc = 'OpenZenFlatpak'
+vim.g.mkdp_refresh_slow = 1
+  end,
 }
